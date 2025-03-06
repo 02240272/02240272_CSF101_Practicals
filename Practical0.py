e139984d841e2cac82db66eabb6887fd7f169aea
Sonam Zam <zams5363@gmail.com>
2025-03-07 03:26:54 +0600

    My Second commit

--- Practical0.py
+++ Practical0.py
@@ -2,3 +2,7 @@ studentName = "Sonam Choden"
 studentNumber = "02240122"
 
 print("My name is " + studentName + " and my student number is " + studentNumber)
+studentName = "Jigme Zangmo"
+studentNumber = "02190102"
+
+print("My name is " + studentName + " and my student number is " + studentNumber)
